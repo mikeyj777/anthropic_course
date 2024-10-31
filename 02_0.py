ed17c3404f1bc01ec8dbf8e1d4a7945f399e7318
Michael James <mikeyj777@gmail.com>
2024-10-31 13:51:39 -0400

    02

--- 02_0.py
+++ 02_0.py
@@ -21,3 +21,46 @@ def get_completion(prompt: str, system_prompt=""):
         ]
     )
     return message.content[0].text
+
+# Exercise 2.1 - Spanish
+# Modify the SYSTEM_PROMPT to make Claude output its answer in Spanish.
+
+
+# System prompt - this is the only field you should change
+SYSTEM_PROMPT = "Your responses should be in proper Spanish.  The kind they speak on Univision that no one understand because Spanish is so differentiated."
+
+# Prompt
+# PROMPT = "Hello Claude, how are you?"
+
+# # Get Claude's response
+# response = get_completion(PROMPT, SYSTEM_PROMPT)
+
+# # Function to grade exercise correctness
+# def grade_exercise(text):
+#     return "hola" in text.lower()
+
+# # Print Claude's response and the corresponding grade
+# print(response)
+# print("\n--------------------------- GRADING ---------------------------")
+# print("This exercise has been correctly solved:", grade_exercise(response))
+
+
+
+# Exercise 2.2 - One Player Only
+# Modify the PROMPT so that Claude doesn't equivocate at all and responds with ONLY the name of one specific player, with no other words or punctuation.
+
+
+# Prompt - this is the only field you should change
+PROMPT = "Skip the preamble.  Only return the name.  No punctuation. While there are multiple contenders for the top, list the most likely best basketball player ever."
+
+# Get Claude's response
+response = get_completion(PROMPT)
+
+# Function to grade exercise correctness
+def grade_exercise(text):
+    return text == "Michael Jordan"
+
+# Print Claude's response and the corresponding grade
+print(response)
+print("\n--------------------------- GRADING ---------------------------")
+print("This exercise has been correctly solved:", grade_exercise(response))
