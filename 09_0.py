c8069cfa7d64d69f7842b16a1a49ab0a5ab4f994
Michael James <mikeyj777@gmail.com>
2024-11-01 19:56:13 -0400

    8 and 9

---
+++ 09_0.py
@@ -0,0 +1,15 @@
+import re
+from model import MODEL_NAME, client
+
+def get_completion(prompt: str, system_prompt="", prefill=""):
+    message = client.messages.create(
+        model=MODEL_NAME,
+        max_tokens=2000,
+        temperature=0.0,
+        system=system_prompt,
+        messages=[
+          {"role": "user", "content": prompt},
+          {"role": "assistant", "content": prefill}
+        ]
+    )
+    return message.content[0].text
